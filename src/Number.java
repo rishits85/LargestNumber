47e207dd059a972a0809ce35cefd0278515ccc49
rishits85 <rishits85@gmail.com>
2015-10-14 18:46:44 -0500

    Completed Project
    This is the complete project for finding largest number

--- src/Number.java
+++ src/Number.java
@@ -9,15 +9,42 @@ public class Number {
 	}
 	
 	public static void getNumber(){
-		for(int i=0; i<10; i++){
+		int NumbersArray[] = new int[11];
+		int largestNumber = NumbersArray[0];
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter number " + i );
-		int NumbersArray[] = {};
-		int no1 = scanner.nextInt();
-		NumbersArray[i] = no1;
-		System.out.println(NumbersArray);
+		for(int i=1; i<11; i++){
+			System.out.println("Enter number " + i );
+			int no1 = scanner.nextInt();
+			NumbersArray[i] = no1;
+			
 		}
-	}
-	
+		scanner.close();
 
+		for(int i=1; i<11; i++)
+		{		
+			  if(NumbersArray[i] > largestNumber)
+				  largestNumber = NumbersArray[i];
+		}
+		System.out.println("the largest number is " + largestNumber);
+	}
 }
+		
+
+//	public void LargestNumber(){
+//		int num1;
+//		int num2;
+//		int largestNumber;
+//		for(int i=1; i<11; i++){
+//			num = NumbersArray[i];
+//			for(int j=1; j<11; j++){
+//				num2 = NumbersArray[i];
+//				if(num1>num2){
+//					largestNumber = num1;
+//				} else{
+//					largestNumber = num2;
+//				}
+//			}
+//		}
+//	}
+
+
